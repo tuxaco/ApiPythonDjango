1dbc5004e67bd480102a1110368f04edce7dcccf
Alberto Castillo García <alberto@castillogarcia.es>
2022-01-04 23:15:33 +0100

    Django Simple API

---
+++ countryapi/countries/serializers.py
@@ -0,0 +1,9 @@
+# countries/serializers.py
+
+from rest_framework import serializers
+from .models import Country
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ["id","name","capital","area"]
---
+++ countryapi/countries/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Country
+from .serializers import CountrySerializer
+
+# Create your views here.
+class CountryViewSet(viewsets.ModelViewSet):
+    serializer_class = CountrySerializer
+    queryset = Country.objects.all()
